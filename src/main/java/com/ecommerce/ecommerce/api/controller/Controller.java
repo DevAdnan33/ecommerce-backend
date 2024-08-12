54ac30f93b7da14646c96cdd18d19edfec51251a
Adnan Rashid <adnanrashid1111@gmail.com>
2024-08-12 21:21:04 +0500

    added login endpoint and JWT authentication with password encryption

--- src/main/java/com/ecommerce/ecommerce/api/controller/Controller.java
+++ src/main/java/com/ecommerce/ecommerce/api/controller/Controller.java
@@ -3,11 +3,13 @@ package com.ecommerce.ecommerce.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ecommerce.ecommerce.api.model.LoginBody;
+import com.ecommerce.ecommerce.api.model.LoginResponse;
 import com.ecommerce.ecommerce.api.model.RegistrationBody;
 import com.ecommerce.ecommerce.exceptions.UserAlreadyExistsException;
 import com.ecommerce.ecommerce.service.UserService;
@@ -22,14 +24,26 @@ public class Controller {
     private UserService userService;
 
     @SuppressWarnings("rawtypes")
-    @GetMapping("/register")
+    @PostMapping("/register")
     public ResponseEntity registerUser(@Valid @RequestBody RegistrationBody registrationBody) {
         try {
             userService.registerUser(registrationBody);
             return ResponseEntity.ok().build();
         } catch (UserAlreadyExistsException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        } 
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> loginUser(@Valid @RequestBody LoginBody loginBody) {
+        String jwt = userService.loginUser(loginBody);
+        if (jwt == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } else {
+            LoginResponse response = new LoginResponse();
+            response.setJwt(jwt);
+            return ResponseEntity.ok(response);
+        }
     }
 
 }
--- src/main/java/com/ecommerce/ecommerce/service/UserService.java
+++ src/main/java/com/ecommerce/ecommerce/service/UserService.java
@@ -1,7 +1,10 @@
 package com.ecommerce.ecommerce.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ecommerce.ecommerce.api.model.LoginBody;
 import com.ecommerce.ecommerce.api.model.RegistrationBody;
 import com.ecommerce.ecommerce.entities.LocalUser;
 import com.ecommerce.ecommerce.exceptions.UserAlreadyExistsException;
@@ -13,6 +16,12 @@ public class UserService {
     @Autowired
     private LocalUserRepo localUserRepo;
 
+    @Autowired
+    private EncryptionService encryptionService;
+
+    @Autowired
+    private JWTService jwtService;
+
     public LocalUser registerUser(RegistrationBody registrationBody) throws UserAlreadyExistsException {
 
         if (localUserRepo.findByUsernameIgnoreCase(registrationBody.getUsername()).isPresent()
@@ -23,11 +32,23 @@ public class UserService {
         LocalUser user = new LocalUser();
         user.setUsername(registrationBody.getUsername());
         user.setEmail(registrationBody.getEmail());
-        user.setPassword(registrationBody.getPassword());
+        user.setPassword(encryptionService.encryptPassword(registrationBody.getPassword()));
         user.setFirstName(registrationBody.getFirstName());
         user.setLastName(registrationBody.getLastName());
 
         return localUserRepo.save(user);
+    }
+
+    public String loginUser(LoginBody loginBody) {
+        Optional<LocalUser> opUser = localUserRepo.findByUsernameIgnoreCase(loginBody.getUsername());
+        if (opUser.isPresent()) {
+            LocalUser user = opUser.get();
+            if (encryptionService.verifyPassword(loginBody.getPassword(), user.getPassword())) {
+                return jwtService.generateJWT(user);
+            }
+        }
+
+        return null;
     }
 
 }
